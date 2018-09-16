6fe49fcb38acd2c1f150d7ee5c837c768cac0ad4
kharrison34 <kyle.harrison344@gmail.com>
2018-09-15 20:26:20 -0400

    workig game

---
+++ click/src/images.js
@@ -0,0 +1,23 @@
+import andersen from './images/andersen.png';
+import matthews from './images/matthews.png';
+import gardiner from './images/gardiner.png';
+import kadri from './images/kadri.png';
+import marleau from './images/marleau.png';
+import marner from './images/marner.png';
+import nylander from './images/nylander.png';
+import rielly from './images/rielly.png';
+import tavares from './images/tavares.png';
+
+const images = [
+  andersen,
+  gardiner,
+  kadri,
+  marleau,
+  marner,
+  matthews,
+  nylander,
+  rielly,
+  tavares
+];
+
+export default images;
